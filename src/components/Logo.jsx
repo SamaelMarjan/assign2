0ca2ec32ea85017704b771e32f7e04d1c51e1da2
Samael Marjan <samaaelmarjan@gmail.com>
2022-10-02 03:07:07 +0600

    update-oct-02-03-07

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Img from '../assets/Rectangle 6.png';
-import Img2 from '../assets/logo1-removebg-preview.png'
+import Img2 from '../assets/log.png';
 
 const Logo = () => {
   return (
@@ -10,9 +10,10 @@ const Logo = () => {
             <img src={Img} alt='img' />
         </div>
         <div className='absolute top-[50%] right-[20%] text-[#fff]'>
-          <img src={Img2} alt='img' />
+            
             <p className='text-[#fff] text-2xl'>Log In</p>
         </div>
+        <img src={Img2} alt='img' className='absolute top-[20px] right-8 w-[50px] h-[50px]' />
     </div>
   );
 }
